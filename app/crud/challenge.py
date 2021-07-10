af261ed6f1cbb8c891651a9c7e02e4d19d19255b
thisisel <1997.esm@gmail.com>
2021-07-11 01:01:10 +0430

    :zap: Add  class RetrieveChallenge

--- app/crud/challenge.py
+++ app/crud/challenge.py
@@ -1,5 +1,7 @@
+from typing_extensions import Annotated
 from app.db.models import ChallengeModel
 from app.schemas.challenge import CreateNewChallenge
+from tortoise.functions import Count
 
 
 class CreateChallenge:
@@ -10,4 +12,21 @@ class CreateChallenge:
         return await ChallengeModel.create(
             **data.dict(exclude_unset=True),
             created_by_id=user_id,
+        )
+
+
+class RetrieveChallenge:
+    @classmethod
+    async def fetch_trending_challenges(cls):
+        return (
+            ChallengeModel.all()
+            .only("id", "title", "duration")
+            .annotate(participants_count=Count("participants"))
+            .order_by("participants_count")
+        )
+
+    @classmethod
+    async def fetch_single_challenge(cls, id: int):
+        return ChallengeModel.get_or_none(id=id).annotate(
+            participants_count=Count("participants")
         )
