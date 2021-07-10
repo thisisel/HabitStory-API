a644d5a9b4d5ee84e4f877fa0234fda28d025034
thisisel <1997.esm@gmail.com>
2021-07-11 01:02:13 +0430

    :zap: Add CHALLANGE_404 code to errors init

--- app/api/errors/__init__.py
+++ app/api/errors/__init__.py
@@ -3,4 +3,4 @@ from . validation_error import http422_error_handler
 from . internal_error import InternalError, internal_error_handler
 from . not_found_error import NotFound, notfound_error_handler
 from .forbidden_handler import Forbidden, forbidden_error_handler
-from . error_categories import USER_404, JOURNAL_404, PAGE_404, REWARD_404, STORY_404, OWNER_AUTH
+from . error_categories import USER_404, JOURNAL_404, PAGE_404, REWARD_404, STORY_404, OWNER_AUTH, CHALLENGE_404
