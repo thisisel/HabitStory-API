51d2635a75811770661715bbfe67e00ae797bd3a
thisisel <1997.esm@gmail.com>
2021-07-11 08:49:15 +0430

    :fire: Remove redundant comments

--- app/api/routes/challenges.py
+++ app/api/routes/challenges.py
@@ -1,6 +1,4 @@
-from app.schemas import challenge
 from app.schemas.challenge import (
-    ChallengeBase_Pydantic,
     ChallengeDetail,
     ChallengeInList,
     SingleChallengeResponse,
@@ -15,20 +13,21 @@ router = APIRouter()
 
 @router.get("")
 async def get_trending_challenges():
+    
     challenges_qset = await RetrieveChallenge.fetch_trending_challenges()
-    # return await challenges_qset
     challenges_obj = await challenges_qset
-    # return await ChallengeBase_Pydantic.from_queryset(challenges_qset)
+   
     return await ChallengeInList.from_queryset(challenges_qset)
 
 
 @router.get("/{id}")
 async def get_single_challenge(id: int = Path(...)):
+    
     challenge_qset = await RetrieveChallenge.fetch_single_challenge(id=id)
 
     if (challenge_obj := await challenge_qset) is None:
         raise NotFound(category=CHALLENGE_404)
-    # return await ChallengeDetail.from_tortoise_orm(challenge_obj)
+
     return SingleChallengeResponse(
         status=True,
         message="challenge sent succ",
