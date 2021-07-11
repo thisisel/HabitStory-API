4411e08ae57e4a315adbf469ade93b42041f9992
thisisel <1997.esm@gmail.com>
2021-07-11 09:20:48 +0430

    :sparkles: Add pagination to trending challenges response

--- app/api/routes/challenges.py
+++ app/api/routes/challenges.py
@@ -5,23 +5,23 @@ from app.schemas.challenge import (
 )
 from app.crud.challenge import RetrieveChallenge
 from app.api.errors import NotFound, CHALLENGE_404
-from fastapi import APIRouter, Path
-from fastapi_pagination.default import Page, Params
+from fastapi import APIRouter, Path, Depends
+from fastapi_pagination import paginate, Page, Params
 
 router = APIRouter()
 
 
-@router.get("")
-async def get_trending_challenges():
-    
+@router.get("", response_model=Page[ChallengeInList])
+async def get_trending_challenges(params: Params = Depends()):
+
     challenges_qset = await RetrieveChallenge.fetch_trending_challenges()
-   
-    return await ChallengeInList.from_queryset(challenges_qset)
+
+    return paginate(await ChallengeInList.from_queryset(challenges_qset), params=params)
 
 
 @router.get("/{id}")
 async def get_single_challenge(id: int = Path(...)):
-    
+
     challenge_qset = await RetrieveChallenge.fetch_single_challenge(id=id)
 
     if (challenge_obj := await challenge_qset) is None:
