07705dc002df7600306c85bc054c50552fc2636b
thisisel <1997.esm@gmail.com>
2021-07-14 08:57:27 +0430

    :bug: Fix streak faulty reset

--- app/utils/journal.py
+++ app/utils/journal.py
@@ -3,7 +3,11 @@ from app.db.models import PageModel
 
 def evaluate_streak(new_page: PageModel, last_page: PageModel, current_streak: int)-> int:
     delta = new_page.submitted - last_page.submitted
+    new_streak = current_streak
     
     if delta.days == 1 :
-        return current_streak + 1
-    return 0
+        new_streak += 1
+    if delta.days >= 3:
+        new_streak = current_streak -1 if current_streak != 0 else 0
+   
+    return new_streak
