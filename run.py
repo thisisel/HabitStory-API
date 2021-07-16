8ed97204e77a32955ac516b6ae2582951c79ea0a
thisisel <1997.esm@gmail.com>
2021-07-16 16:50:04 +0430

    :recycle: Add host and port as optional enviorment variables

--- run.py
+++ run.py
@@ -1,9 +1,12 @@
+import os
 import uvicorn
 
 from app import app
 
 def run():
-    uvicorn.run("run:app", host="0.0.0.0", port=8000, reload=True)
+    host = os.getenv('HOST', "0.0.0.0")
+    port = os.getenv('PORT', 8000)
+    uvicorn.run("run:app", host=host, port=port, reload=True)
 
 if __name__ == '__main__':
     run()
